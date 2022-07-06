8de2bd31f775a18732ddba0bf06d79060e6cef3f
Jess Hause Ugray <jess.ugray@gmail.com>
2022-07-06 10:48:52 -0400

    github url data fetched and returned as json

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,25 @@
 import { Router, Request, Response } from 'express';
+import fetch from 'node-fetch';
+import { json } from 'stream/consumers';
 
 export const repos = Router();
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
-
-  res.status(200);
+  fetch('https://api.github.com/users/silverorange/repos')
+    .then((apiResponse) => {
+      const status = apiResponse.status;
+      if (status !== 200) {
+        res.status(status);
+        console.log(`Error: ${status}`);
+        return;
+      }
+      return apiResponse.json();
+    }).then(jsonResult => {
+      res.json(jsonResult)
+    })
+    
+    
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
 });
